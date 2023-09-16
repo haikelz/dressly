86c03f597ff0b233b87acb6a5d7062a0603c8fa6
haikelz <haikel.2017th@gmail.com>
2023-09-16 21:45:56 +0700

    fix: text position

--- app/components/top-selections.tsx
+++ app/components/top-selections.tsx
@@ -4,7 +4,7 @@ export default function TopSelections() {
   return (
     <Layout>
       <section className="w-full py-8 md:py-20">
-        <div className="w-full flex justify-center sm:justify-start sm:items-start items-center flex-col">
+        <div className="w-full flex justify-start items-start flex-col">
           <h2 className="text-neutral-900 text-2xl font-semibold font-lora md:text-5xl">
             Top Selections
           </h2>
